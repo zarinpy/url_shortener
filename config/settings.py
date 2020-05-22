98d2b950ac62b2684eb9987456ce66839c98d1c0
omid zarinmahd <zarinpy@gmail.com>
2020-05-23 00:45:12 +0430

    static setting, add gitignore items, optimize imports

--- config/settings.py
+++ config/settings.py
@@ -98,7 +98,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), '.static']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -176,3 +176,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '.static'
--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -13,6 +16,9 @@ api_v1 = [
     path('home/', include('short_url.api.v1.urls'), name='url_creator'),
 ]
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/v1/', include((api_v1, 'url_shortener'), namespace='url_shortener')),
     path(r'r/<str:input_url>/', RedirectToView.as_view(), name='shorted'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
